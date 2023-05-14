a07437610f59d5d0363c21ed6229ce41b8101f69
alegrance <alegrance1000@gmail.com>
2023-05-14 11:50:20 -0400

    add caja formgroup

--- src/app/pages/configuracion/configuracion.component.ts
+++ src/app/pages/configuracion/configuracion.component.ts
@@ -40,6 +40,11 @@ export class ConfiguracionComponent implements OnInit {
 
   // Forms
   public usuarioForm: any;
+  public usuarioEditForm: any;
+
+  public cajaForm: any;
+  public cajaEditForm: any;
+
 
   ngOnInit(): void {
     this.getUsuarioLogeadoRole();
@@ -80,6 +85,72 @@ export class ConfiguracionComponent implements OnInit {
         Validators.minLength(4)
       ])
     });
+
+    // Form EDIT Usuario
+    this.usuarioEditForm = new FormGroup({
+      user_name_edit: new FormControl('user', [
+        Validators.required,
+        Validators.minLength(4)
+      ]),
+      user_fullname_edit: new FormControl('Usuario', [
+        Validators.required,
+        Validators.minLength(4)
+      ]),
+      user_password_edit: new FormControl('12345', [
+        Validators.required,
+        Validators.minLength(4)
+      ]),
+      user_email_edit: new FormControl('user@ventaxpress.com', [
+        Validators.required,
+        Validators.minLength(4)
+      ])
+    });
+
+    // Form ADD Caja
+    this.cajaForm = new FormGroup({
+      fecha_apertura: new FormControl('', [
+        Validators.required,
+        Validators.minLength(4)
+      ]),
+      fecha_cierre: new FormControl('', [
+        Validators.required,
+        Validators.minLength(4)
+      ]),
+      total_ventas: new FormControl(0, [
+        Validators.required,
+        Validators.minLength(1)
+      ]),
+      total_devoluciones: new FormControl(0, [
+        Validators.required,
+        Validators.minLength(1)
+      ]),
+      total_efectivo: new FormControl(0, [
+        Validators.required,
+        Validators.minLength(1)
+      ])
+      ,
+      total_tarjeta: new FormControl(0, [
+        Validators.required,
+        Validators.minLength(1)
+      ])
+      ,
+      total_cheques: new FormControl(0, [
+        Validators.required,
+        Validators.minLength(1)
+      ]),
+      total_cupones: new FormControl(0, [
+        Validators.required,
+        Validators.minLength(1)
+      ]),
+      cambio_inicial: new FormControl(0, [
+        Validators.required,
+        Validators.minLength(1)
+      ]),
+      cambio_final: new FormControl(0, [
+        Validators.required,
+        Validators.minLength(1)
+      ])
+    });
   }
 
   // Validaciones para ADD Usuario
@@ -88,7 +159,19 @@ export class ConfiguracionComponent implements OnInit {
   get password() { return this.usuarioForm.get('user_password'); }
   get email() { return this.usuarioForm.get('user_email'); }
 
-  submit() {
+  // Validaciones para ADD Caja
+  get fechaApertura() { return this.cajaForm.get('fecha_apertura'); }
+  get fechaCierre() { return this.cajaForm.get('fecha_cierre'); }
+  get totalVentas() { return this.cajaForm.get('total_ventas'); }
+  get totalDevoluciones() { return this.cajaForm.get('total_devoluciones'); }
+  get totalEfectivo() { return this.cajaForm.get('total_efectivo'); }
+  get totalTarjetas() { return this.cajaForm.get('total_tarjeta'); }
+  get totalCheques() { return this.cajaForm.get('total_cheques'); }
+  get totalCupones() { return this.cajaForm.get('total_cupones'); }
+  get cambioInicial() { return this.cajaForm.get('cambio_inicial'); }
+  get CambioFinal() { return this.cajaForm.get('cambio_final'); }
+
+  submitUsuario() {
     let objUsuario = {
       user_name: this.usuarioForm.get('user_name').value,
       user_fullname: this.usuarioForm.get('user_fullname').value,
@@ -110,6 +193,50 @@ export class ConfiguracionComponent implements OnInit {
           this.getAllData();
           // Funcion para resetear el formulario
           this.usuarioForm.reset();
+        }
+
+        if (result.status === 'error') {
+          this.toastr.error(result.body[0].message, 'Error');
+          //console.log('Error', result);
+        }
+
+        //this.usuarios = data;
+        console.log(data);
+      }, error => {
+        console.log(error);
+        this.toastr.error(error.message, `Server ERROR: ${error.status}`);
+      })
+  }
+
+  submitCaja() {
+    let objCaja = {
+      fecha_apertura: this.cajaForm.get('fecha_apertura').value,
+      fecha_cierre: this.cajaForm.get('fecha_cierre').value,
+      total_ventas: this.cajaForm.get('total_ventas').value,
+      total_devoluciones: this.cajaForm.get('total_devoluciones').value,
+      total_efectivo: this.cajaForm.get('total_efectivo').value,
+      total_tarjeta: this.cajaForm.get('total_tarjeta').value,
+      total_cheques: this.cajaForm.get('total_cheques').value,
+      total_cupones: this.cajaForm.get('total_cupones').value,
+      cambio_inicial: this.cajaForm.get('cambio_inicial').value,
+      cambio_final: this.cajaForm.get('cambio_final').value,
+
+      user_id: 1
+    };
+
+    console.log(objCaja);
+
+    this.api.post('caja', objCaja)
+      .subscribe(data => {
+        let result: any = data;
+
+        if (result.status === 'success') {
+          this.toastr.success('Caja registrado');
+          //console.log('Success', result);
+          // Llama a la funcion onInit que agrega a la lista el cliente registrado
+          this.getAllData();
+          // Funcion para resetear el formulario
+          this.cajaForm.reset();
         }
 
         if (result.status === 'error') {
